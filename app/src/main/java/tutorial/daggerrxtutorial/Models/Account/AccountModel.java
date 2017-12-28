9597d58d6c54991d9f45efcf92c643b608d00a55
lorence <lorence@enclave.vn>
2017-12-28 11:16:24 +0700

    Structure app

---
+++ app/src/main/java/tutorial/daggerrxtutorial/Models/Account/AccountModel.java
@@ -0,0 +1,8 @@
+package tutorial.daggerrxtutorial.Models.Account;
+
+/**
+ * Created by lorence on 28/12/2017.
+ */
+
+public class AccountModel {
+}
---
+++ app/src/main/java/tutorial/daggerrxtutorial/Presenters/Account/IAccountModel.java
@@ -0,0 +1,8 @@
+package tutorial.daggerrxtutorial.Presenters.Account;
+
+/**
+ * Created by lorence on 28/12/2017.
+ */
+
+public interface IAccountModel {
+}
